a75d61f2b9ff54bb1de53ee29c54422c2d31a9a0
Alston Godbolt <alstongodbolt@Alstons-MacBook-Air.local>
2015-08-28 10:49:34 -0500

    Initial Commit

---
+++ Chapter 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter 2
+//
+//  Created by Alston Godbolt on 8/28/15.
+//  Copyright (c) 2015 Alston Godbolt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
