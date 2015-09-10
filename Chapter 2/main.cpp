a7c62b8ba7cc079476175ef4863a3d6f15195d9b
alstongodbolt <godbolta@onid.oregonstate.edu>
2015-09-10 16:02:44 -0500

    Chapter 2
    Programming challenges for the text book “Starting Out with C++ Early
    Objects” 7th edition

--- Chapter 2/main.cpp
+++ Chapter 2/main.cpp
@@ -7,9 +7,189 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    //cout << "Hello, World!";
+    // sum of two numbers
+    /*
+    int x = 62;
+    int y = 99;
+    
+    int total = x + y;
+    
+    cout << total << endl;
+    
+    
+    //Sales Prediction
+    
+    cout << "The East Cost sales division generates 62% of total sales, below we will find out what 62% of $4.6 million is";
+    double percent = .62;
+    double total_sales = 4600000;
+    
+    cout << "62% of $4.6 Million is: $" << percent * total_sales;
+    
+    
+    //Sales tax
+    
+    double purchase = 52;
+    double state_sales = .04;
+    double county_sales = .02;
+    
+    cout << "Item price: $52.00" << endl;
+    cout << "State sales tax: 4%" << endl;
+    cout << "What is the state sales tax on the item purchased? $";
+    state_sales = purchase * state_sales;
+    cout << state_sales << endl;
+    cout << "County sales tax: 2%" << endl;
+    cout << "What is the county sales tax on the item purchased? $";
+    county_sales = purchase * county_sales;
+    cout << county_sales << endl;
+    purchase = purchase + state_sales + county_sales;
+    
+    cout << "What is the total cost of this purchase? $" << purchase << endl;
+    
+    
+    //Resturant Bill
+    
+    double meal = 44.50;
+    double tax = .0675;
+    double tip = .15;
+    
+    cout << "Cost of the meal $" << meal << endl;
+    tax = tax * meal;
+    cout << "Tax on the meal $" << tax << endl;
+    tip = (meal + tax) * tip;
+    cout << "The tip amount (15%) will be $" << tip << endl;
+    meal = meal + tax + tip;
+    cout << "The total bill is $" << meal << endl;
+    
+    //Cyborg Data Types
+    
+    cout << "The size of data type char is: " << sizeof(char) << endl;
+    cout << "The size of data type int is: " << sizeof(int) << endl;
+    cout << "The size of data type float is: " << sizeof(float) << endl;
+    cout << "The size of data typr double is: " << sizeof(double) << endl;
+    
+    
+    //Miles per Gallon
+    
+    double gallons = 16;
+    double miles = 350;
+    double travel = miles / gallons;
+    
+    cout << "The car can travel " << travel << " before refueling." << endl;
+    
+    
+    //Distacne per Tank of Gas
+    
+    double tank;
+    double town;
+    double highway;
+    
+    tank = 20;
+    town = 21.5;
+    highway = 26.8;
+    
+    town = town * tank;
+    highway = highway * tank;
+    cout << "The car can travel " << town << " miles in town, or " << highway << " miles on the highway, but not both." << endl;
+    
+    
+    // Land Calculation
+    
+    
+    double square_feet = 43560;
+    double square_meter = 10.7639;
+    double quarter_acre = .25;
+    
+    square_feet = square_feet / quarter_acre;
+    square_meter = square_meter / quarter_acre;
+    
+    cout << "The number of square feet in a quarter mile is " << square_feet << endl;
+    cout << "The number of square meters in a quarter mile is " << square_meter << endl;
+    
+    
+    //Circuit Board Price
+    
+    double profit = .40;
+    double cost = 12.67;
+    profit = profit * cost;
+    profit = profit + cost;
+    
+    cout << "The circuit boards must be sold at $" << profit << " to make a 40% profit on each one sold" << endl;
+    
+    
+    //Personal Information
+    
+    cout << "Name: Alston Godbolt\nAddress: 11420 60th Place\n         Kenosha, WI 53142\nPhone Number: (262) 515-0052\nCollege Major: Computer Science";
+    
+    
+    //Triangle Pattern
+    
+    cout << "   *\n";
+    cout << "  ***\n";
+    cout << " *****\n";
+    cout << "*******\n";
+    
+    //Diamond Pattern
+    
+    cout << "   *\n";
+    cout << "  ***\n";
+    cout << " *****\n";
+    cout << "*******\n";
+    cout << " *****\n";
+    cout << "  ***\n";
+    cout << "   *\n";
+    
+    //Pay Period Pay
+    
+    double annual = 32500;
+    double month = 24;
+    double bi_weekly = 26;
+    month = annual / month;
+    bi_weekly = annual / bi_weekly;
+    
+    cout << "If an employee is paid annually his gross pay would be $" << annual << endl;
+    cout << "\nIf an employee was paid twice per month, his gross pay would be $" << month << endl;
+    cout << "\nIf an employee was paid bi-weekly, his gross pay would be $" << bi_weekly << endl;
+    
+    
+    //Basketball Player Height
+    
+    int star = 73;
+    int height = 12;
+    star = star / height;
+    int inches = star % height;
+    
+    cout << "The start player is " << star << " foot " << inches << " inches tall.";
+    
+    
+    //Stock Loss
+    
+    double shares = 600;
+    double purchase_price = 21.77;
+    purchase_price = purchase_price * shares;
+    double selling_price = 16.44;
+    selling_price = selling_price * shares;
+    double total_lost = selling_price - purchase_price;
+    
+    cout << "Kathryn purchased 600 shares at $21.77 per share. Kathryn paid a total of $" << purchase_price << endl;
+    cout << "\nLater, Kathryn sold her 600 shares at $16.44 per share for a total of $" << selling_price << endl;
+    cout << "\nKathryn lost a total of $" << total_lost << " on her failed investment";
+    
+    
+    //Energy drink consumption
+    double total_surveed = 12467;
+    double one_or_more = .14;
+    double citrus = .64;
+    one_or_more = total_surveed * one_or_more;
+    citrus = citrus * one_or_more;
+    
+    cout << "The number of customers in the survey who purchase one or more energy drinks per week is: " << one_or_more << endl;
+    cout << "\nThe number of customers in the survey who perfer citrus flavored energy drinks is: " << citrus << endl;
+    */
+    
     return 0;
 }
